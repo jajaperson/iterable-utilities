37d113d3cff14a42b7b5aa30be824b44d9af7186
James Jensen <jamesarthurjensen@gmail.com>
2021-01-21 16:51:12 +0800

    Improve documentation

--- lib/combinators.ts
+++ lib/combinators.ts
@@ -1,11 +1,16 @@
 /**
  * Creates a new iterator containing tuples of each element of `it1` and `it2`.
- * @param it1 Iterator to be mapped to the first element of each tuple in the new iterator.
- * @param it2 Iterator to be mapped to the second element of each tuple in the new iterator.
+ * @param it1 - Iterator to be mapped to the first element of each tuple in the
+ * new iterator.
+ * @param it2 - Iterator to be mapped to the second element of each tuple in the
+ * new iterator.
+ * @typeParam T - Type of items in `it1`
+ * @typeParam U - Type of items in `it2`
+ * @returns An iterator containing pairs of items taken from `it1` and `it2`
  */
 export function* pair<T, U>(
   it1: Iterable<T>,
-  it2: Iterable<U>,
+  it2: Iterable<U>
 ): IterableIterator<[T, U]> {
   const iterator1 = it1[Symbol.iterator]();
   const iterator2 = it2[Symbol.iterator]();
@@ -21,9 +26,13 @@ export function* pair<T, U>(
 
 /**
  * Combines two or more iterators.
- * @param head The first iterator.
- * @param tails Additional iterators to add to the end of head.
+ * @param head - The first iterator.
+ * @param tails - (blob) Additional iterators to add to the end of head.
  * If there are more than one, they must be of the same type.
+ * @typeParam T - The item type of `head`.
+ * @typeParam U - The item type of each tail.
+ * @returns An iterator which yields items from the head followed by items from
+ * the tail(s) in order.
  */
 export function* concat<T, U = T>(
   head: Iterable<T>,
--- lib/generators.ts
+++ lib/generators.ts
@@ -1,13 +1,23 @@
-import { kComb } from "./util.ts";
+import { kComb } from "./internal/util.ts";
+
+/**
+ * {@link from | `from`} generator callback.
+ * @callback FromCallback
+ * @param index - The current index.
+ */
+export interface FromCallback<T> {
+  (index: number): T;
+}
 
 /**
  * Creates an iterator from the return values of a function.
- * @param f A function which optionally takes the index as an argument and
- * returns the next item in the iterator,
+ * @param {FromCallback} f - A function which optionally takes the index as an
+ * argument and
+ * returns the next item in the iterator.
+ * @typeParam T - The return type of `f`, and thus the item type of the new
+ * iterator
  */
-export function* from<T>(
-  f: (() => T) | ((i: number) => T),
-): IterableIterator<T> {
+export function* from<T>(f: FromCallback<T>): IterableIterator<T> {
   let index = 0;
   while (true) {
     yield f(index);
@@ -17,6 +27,7 @@ export function* from<T>(
 
 /**
  * Creates an endless iterable of pseudorandom numbers.
+ * @returns An iterator containing lazily calculated pseudorandom numbers.
  */
 export function* randomNumbers(): IterableIterator<number> {
   yield* from(Math.random);
@@ -25,6 +36,7 @@ export function* randomNumbers(): IterableIterator<number> {
 /**
  * Creates an endless iterable of a constant value.
  * @param value The value of all items in the returned iterator.
+ * @returns An endless iterator of `value`.
  */
 export function* constant<T>(value: T): IterableIterator<T> {
   yield* from(kComb(value));
