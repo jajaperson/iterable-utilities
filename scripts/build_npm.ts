b82f171c1dbba62886b901b0acf5920ae7c21a4b
James Jensen <jamesarthurjensen@gmail.com>
2022-08-28 17:08:20 +0800

    ci: Fix `dnt` version

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,4 +1,4 @@
-import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+import { build, emptyDir } from "https://deno.land/x/dnt@0.29.1/mod.ts";
 import { LICENSE as license, VERSION as version } from "../version.ts";
 
 await emptyDir("./npm");
